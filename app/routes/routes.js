5df738706d83f8354f3f81cdc1346870ba993607
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-11-01 19:42:45 -0300

    f

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,7 +4,6 @@ const Movies = require('../controllers/moviesController');
 module.exports = {
     getMovies: (app) =>{
         app.get('/api/filmes', Movies.apiGetAllMovies);
-    
     },
     addMovie: (app) => {
         app.post('/api/filmes', Movies.addMovie);
